b8bba5043ed0ff9e278e3bd061fbc58c358297c1
zuzhi <zuzhi.hu@gmail.com>
2022-01-13 10:19:08 +0800

    update to Resources & Built-In Config

--- hello_cereal.py
+++ hello_cereal.py
@@ -3,7 +3,11 @@ from dagster.core.definitions.output import Out
 from dagster.core.definitions.input import In
 import requests
 import csv
-from dagster import job, op, get_dagster_logger, DagsterType, TypeCheck
+import sqlite3
+from copy import deepcopy
+from dagster import job, op, resource, graph, get_dagster_logger, DagsterType, TypeCheck, Field, String, config_from_files, file_relative_path
+import sqlalchemy
+import sqlalchemy.ext.declarative
 
 
 @op
@@ -152,7 +156,124 @@ def sort_by_calories(cereals):
     )
 
 
+@op(required_resource_keys={"warehouse"})
+def normalize_calories(context, cereals):
+    columns_to_normalize = [
+        "calories",
+        "protein",
+        "fat",
+        "sodium",
+        "fiber",
+        "carbo",
+        "sugars",
+        "potass",
+        "vitamins",
+        "weight",
+    ]
+    quantities = [cereal["cups"] for cereal in cereals]
+    reweights = [1.0 / float(quantity) for quantity in quantities]
 
+    normalized_cereals = deepcopy(cereals)
+    for idx in range(len(normalized_cereals)):
+        cereal = normalized_cereals[idx]
+        for column in columns_to_normalize:
+            cereal[column] = float(cereal[column]) * reweights[idx]
+
+    context.resources.warehouse.update_normalized_cereals(normalized_cereals)
+
+
+class LocalSQLiteWarehouse:
+    def __init__(self, conn_str):
+        self._conn_str = conn_str
+
+    # In practice, you'll probably want to write more generic, reusable logic on your resources
+    # than this tutorial example
+    def update_normalized_cereals(self, records):
+        conn = sqlite3.connect(self._conn_str)
+        curs = conn.cursor()
+        try:
+            curs.execute("DROP TABLE IF EXISTS normalized_cereals")
+            curs.execute(
+                """CREATE TABLE IF NOT EXISTS normalized_cereals
+                (name text, mfr text, type text, calories real,
+                 protein real, fat real, sodium real, fiber real,
+                 carbo real, sugars real, potass real, vitamins real,
+                 shelf real, weight real, cups real, rating real)"""
+            )
+            curs.executemany(
+                """INSERT INTO normalized_cereals VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                [tuple(record.values()) for record in records],
+            )
+        finally:
+            curs.close()
+
+
+@resource(config_schema={"conn_str": Field(String)})
+def local_sqlite_warehouse_resource(context):
+    return LocalSQLiteWarehouse(context.resource_config["conn_str"])
+
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+
+class NormalizedCereal(Base):
+    __tablename__ = "normalized_cereals"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    mfr = sqlalchemy.Column(sqlalchemy.String)
+    type = sqlalchemy.Column(sqlalchemy.String)
+    calories = sqlalchemy.Column(sqlalchemy.Float)
+    protein = sqlalchemy.Column(sqlalchemy.Float)
+    fat = sqlalchemy.Column(sqlalchemy.Float)
+    sodium = sqlalchemy.Column(sqlalchemy.Float)
+    fiber = sqlalchemy.Column(sqlalchemy.Float)
+    carbo = sqlalchemy.Column(sqlalchemy.Float)
+    sugars = sqlalchemy.Column(sqlalchemy.Float)
+    potass = sqlalchemy.Column(sqlalchemy.Float)
+    vitamins = sqlalchemy.Column(sqlalchemy.Float)
+    shelf = sqlalchemy.Column(sqlalchemy.Float)
+    weight = sqlalchemy.Column(sqlalchemy.Float)
+    cups = sqlalchemy.Column(sqlalchemy.Float)
+    rating = sqlalchemy.Column(sqlalchemy.Float)
+
+
+class SqlAlchemyPostgresWarehouse:
+    def __init__(self, conn_str):
+        self._conn_str = conn_str
+        self._engine = sqlalchemy.create_engine(self._conn_str)
+
+
+    def update_normalized_cereals(self, records):
+        Base.metadata.bind = self._engine
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        NormalizedCereal.__table__.insert().execute(records)
+
+
+@resource(config_schema={"conn_str": Field(String)})
+def sqlalchemy_postgres_warehouse_resource(context):
+    return SqlAlchemyPostgresWarehouse(context.resource_config["conn_str"])
+
+
+@graph
+def calories():
+    normalize_calories(download_csv())
+
+# calories_test_job = calories.to_job(
+#     resource_defs={"warehouse": local_sqlite_warehouse_resource},
+# )
+calories_test_job = calories.to_job(
+    resource_defs={"warehouse": local_sqlite_warehouse_resource},
+    config={"ops": {"download_csv": {"config": {"url": "https://docs.dagster.io/assets/cereal.csv"}}},
+            "resources": {"warehouse": {"config": {"conn_str": ":memory:"}}}},
+)
+# calories_dev_job = calories.to_job(
+#     resource_defs={"warehouse": sqlalchemy_postgres_warehouse_resource},
+#     config=config_from_files(
+#         [file_relative_path(__file__, "presets_dev_warehouse.yaml")]
+#     ),
+# )
 
 
 # @job
@@ -173,7 +294,7 @@ def diamond():
     )
 
 
-@job
+# @job
 def configurable_job():
     sort_by_calories(download_csv())
 
@@ -183,6 +304,23 @@ def bad_configurable_job():
     sort_by_calories(bad_download_csv())
 
 
+# @job(resource_defs={"warehouse": local_sqlite_warehouse_resource})
+def resources_job():
+    normalize_calories(download_csv())
+
+
+run_config = {
+        "ops": {
+            "download_csv": {
+                "config": {"url": "https://docs.dagster.io/assets/cereal.csv"}
+            }
+        },
+        "resources": {"warehouse": {"config": {"conn_str": ":memory:"}}}
+    }
+
+
 if __name__ == "__main__":
     # result = hello_cereal_job.execute_in_process()
-    result = configurable_job.execute_in_process(run_config=run_config)
+    # result = configurable_job.execute_in_process(run_config=run_config)
+    # result = calories_test_job.execute_in_process(run_config=run_config)
+    result = calories_test_job.execute_in_process()
